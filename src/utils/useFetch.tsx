d6598a8b218ebe98142effbf9814cb671c1b9e58
Quang Truong <qtruongngoc95@gmail.com>
2024-07-01 14:18:46 +0700

    fix/cleanup-function

--- src/utils/useFetch.tsx
+++ src/utils/useFetch.tsx
@@ -9,36 +9,42 @@ import {
 } from "../redux/page/pageSlice";
 
 export const useFetchGitHub = () => {
-    const dispatch = useDispatch();
-    const { data, loading, error } = useSelector((state: StoreState) => ({
-      data: state.page.data,
-      loading: state.page.loading,
-      error: state.page.error,
-    }));
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        dispatch(fetchingStart());
-        const octokit = new Octokit({
-          auth: `${process.env.REACT_APP_GITHUB_KEY}`,
-        });
-  
-        try {
-          const response = await octokit.request(
-            `GET /users/${process.env.REACT_APP_GITHUB_NAME}`,
-            {
-              headers: {
-                "X-GitHub-Api-Version": "2022-11-28",
-              },
-            }
-          );
-          dispatch(fetchingSuccess(response.data));
-        } catch (error: any) {
-          dispatch(fetchingFailure(error.message));
-        }
-      };
-      fetchData();
-    }, [dispatch]);
-  
-    return { data, loading, error };
-  };
+  const dispatch = useDispatch();
+  const { data, loading, error } = useSelector((state: StoreState) => ({
+    data: state.page.data,
+    loading: state.page.loading,
+    error: state.page.error,
+  }));
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+    const fetchData = async () => {
+      dispatch(fetchingStart());
+      const octokit = new Octokit({
+        auth: `${process.env.REACT_APP_GITHUB_KEY}`,
+      });
+      try {
+        const response = await octokit.request(
+          `GET /users/${process.env.REACT_APP_GITHUB_NAME}`,
+          {
+            headers: {
+              "X-GitHub-Api-Version": "2022-11-28",
+            },
+            signal,
+          }
+        );
+        dispatch(fetchingSuccess(response.data));
+      } catch (error: any) {
+        dispatch(fetchingFailure(error.message));
+      }
+    };
+    fetchData();
+    return () => {
+      // Cleanup function
+      abortController.abort();
+    };
+  }, [dispatch]);
+
+  return { data, loading, error };
+};
