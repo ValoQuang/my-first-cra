dc4ae190ba3ee88b2220ccb9a38d4ed643e7827f
Quang Truong <qtruongngoc95@gmail.com>
2024-07-01 09:07:33 +0700

    feat/repo-init-structures

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+      <button className="btn btn-lg btn-outline btn-success">Success</button>
+    </div>
+  )
+}
+
+export default Button
